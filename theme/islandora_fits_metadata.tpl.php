4e4c96a7840ca761bba8c83cf01346934153f29e
Jordan Dukart <jordan@discoverygarden.ca>
2013-01-16 15:20:19 -0400

    Added a technical metadata tab accessible through the management of an Islandora object. Created an additional permission to check for access to this tab.

--- theme/islandora_fits_metadata.tpl.php
+++ theme/islandora_fits_metadata.tpl.php
@@ -1,2 +1,22 @@
 <?php
-dd($islandora_fits_metadata);
+
+/**
+ * @file
+ *
+ * This is a template used to display the technical metadata that
+ * is present in the TECHMD datastream.
+ *
+ * islandora_object is a fedora tuque Object. Additional documentation
+ * can be gathered from the islandora-object.tpl.php present in the Islandora
+ * module.
+ *
+ * islandora_fits_table contains the required table definition needed
+ * in rendering a table. It contains the header fields as well as the data
+ * to be placed in rows.
+ * 
+ */
+?>
+
+<?php drupal_set_title($islandora_object->label . ' Technical Metadata'); ?>
+
+<?php print theme('table', $islandora_fits_table); ?>
